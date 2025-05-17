cae3bd661d458f3926e6a4acc3c9384e9992462b
cosmeNicolas <nicomas7590@gmail.com>
2025-05-16 23:50:40 -0300

    la cree como una PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  darkMode: 'class',
   plugins: [
     tailwindcss(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Planillas CEMD',
+        short_name: 'CEMD',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#1f1f1f',
+        theme_color: '#2AB0A1',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
-})
+});
